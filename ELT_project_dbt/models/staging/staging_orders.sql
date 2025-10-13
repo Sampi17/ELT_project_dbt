e7478f713cb9e83e4da4cbcff869f1b9d19ca0d3
Samukelo <samukelomnguni174@gmail.com>
2025-10-13 20:47:42 +0200

    Renaming columns, lowercase column names and fixing data types on orders  table

---
+++ ELT_project_dbt/models/staging/staging_orders.sql
@@ -0,0 +1,16 @@
+{{ 
+    config(
+        materialized='view',
+        alias='staging_orders'
+    )
+}}
+
+--Renaming columns, lowercase column names and fixing data types
+SELECT
+    "ORDERID" AS order_id,
+    "BRANCH_ID" AS branch_id,
+    CAST("DATE_" AS DATE) AS date,
+    "USERID" AS customer_id,
+    CAST("NAMESURNAME" AS VARCHAR(50)) AS fullname,
+    CAST("TOTALBASKET" AS DECIMAL) AS total_basket
+FROM {{source('raw_data','orders')}}
