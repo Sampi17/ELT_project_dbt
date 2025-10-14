cdb06f4a1ca8ddbe95132d47671f022a1caec7a0
Samukelo <samukelomnguni174@gmail.com>
2025-10-14 04:52:19 +0200

    fixed syntax issues

--- ELT_project_dbt/models/marts/superstore_fact.sql
+++ ELT_project_dbt/models/marts/superstore_fact.sql
@@ -5,9 +5,9 @@ SELECT
     o.branch_id AS store_key,
     o.customer_id AS customer_key,
     od.category_id AS category_key,
-    od.product_id AS product_key
+    od.product_id AS product_key,
     od.unit_price,
-    od.total_price
+    od.total_price,
     od.status
 FROM {{ref('staging_order_details')}} od
 LEFT JOIN {{ref('staging_orders')}} o
